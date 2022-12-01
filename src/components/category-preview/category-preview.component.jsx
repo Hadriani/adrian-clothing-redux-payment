2a11bc4eead30d8e5cf9baac3752b6fb76d18275
Adrian Păun <79643678+Hadriani@users.noreply.github.com>
2022-12-01 09:48:08 +0200

    Add files via upload

---
+++ src/components/category-preview/category-preview.component.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom'; 
+
+import ProductCard from '../../components/product-card/product-card.component';
+
+import './category-preview.styles';
+
+import { CategoryPreviewContainer } from './category-preview.styles';
+
+const CategoryPreview = ({ title, products }) => {
+
+    return (
+        <CategoryPreviewContainer>
+            <h2>
+                <Link className='title' to={title}>{title.toUpperCase()}</Link>
+            </h2>
+            <div className = 'preview'>
+                {products.filter((_, idx) => idx < 4).map((products) => (<ProductCard key={products.id} product={products} />
+                ))}
+            </div>
+        </CategoryPreviewContainer>
+    )
+}
+    
+                                   
+
+export default CategoryPreview;
---
+++ src/store/categories/category.action.js
@@ -0,0 +1,14 @@
+import { CATEGORIES_ACTIONS_TYPES } from './category.types';
+
+import { createAction } from '../../utils/reducer/reducer.utils';
+
+
+export const fetchCategoriesStart = () => 
+  createAction(CATEGORIES_ACTIONS_TYPES.FETCH_CATEGORIES_START)
+
+export const fetchCategoriesSuccess = (categoriesArray) =>
+  createAction(CATEGORIES_ACTIONS_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray);
+
+export const fetchCategoriesFailed = (error) =>
+  createAction(CATEGORIES_ACTIONS_TYPES.FETCH_CATEGORIES_FAILED, error);
+
