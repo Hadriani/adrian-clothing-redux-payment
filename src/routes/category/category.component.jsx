becbcba85f3d9b229aa525737330e2a00ba6cb62
Adrian Păun <79643678+Hadriani@users.noreply.github.com>
2022-12-08 00:32:53 +0200

    Add files via upload

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -7,32 +7,33 @@ import Spinner from '../../components/spinner/spinner.component';
 
 import { selectIsLoading, selectCategoriesMap } from '../../store/categories/category.selector';
 
-import './category.styles.scss';
+import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
     const { category } = useParams();
     const categoriesMap = useSelector(selectCategoriesMap);
     const isLoading = useSelector(selectIsLoading);
-    const [ products, setProducts] = useState(categoriesMap[category]);
+    const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(() => {
         setProducts(categoriesMap[category]);
-    }, [category, categoriesMap ])
+    }, [category, categoriesMap]);
 
     return (
         <Fragment>
-            <h2 className='category-title'>{category.toUpperCase}</h2>
-            {
-                isLoading ? ( <Spinner /> ) : ( <div className='category-container'>
+            <Title>{category.toUpperCase()}</Title>
+            {isLoading ? (
+                <Spinner />
+            ) : (
+                <CategoryContainer>
                     {products &&
-                        products.map((product) => ( <ProductCard key={product.id} product={product} />
+                        products.map((product) => (
+                            <ProductCard key={product.id} product={product} />
                         ))}
-                </div>
-                )}
-        
+                </CategoryContainer>
+            )}
         </Fragment>
     );
-
 };
 
 export default Category;
